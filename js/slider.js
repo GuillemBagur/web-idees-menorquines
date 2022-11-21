fcd79c9d5e205e5ae198b09cca6e2b15db42cf4f
Guillem Bagur <guillembagurmoll@gmail.com>
2022-11-21 22:16:45 +0100

    Slider (missing scroll limits

--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,24 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+const getTranslateX = (el) => {
+  const style = window.getComputedStyle(el);
+  const matrix = new WebKitCSSMatrix(style.transform);
+  return matrix.m41;
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+const slideScroll = btn => {
+  const slider = btn.parentElement;
+  const sliderDimensions = slider.getBoundingClientRect();
+  const slides = slider.querySelector(".slider__slides-wrapper");
+  const scrollDir = Number(btn.dataset.direction);
+  const slidesDimensions = slides.getBoundingClientRect();
+  const speed = 150 * slidesDimensions.width / 800;
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slider__slide");
+  const currentTransform = getTranslateX(slides);
+  const newTransform = currentTransform + speed * scrollDir;
+  let translate = newTransform;
 
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
+  //if(sliderDimensions.right > slidesDimensions.right && scrollDir < 0) translate = currentTransform ;
+  //if(sliderDimensions.left < slidesDimensions.left && scrollDir > 0) translate = currentTransform ;
+
+
+  slides.style.transform = `translateX(${translate}px)`;
 }
